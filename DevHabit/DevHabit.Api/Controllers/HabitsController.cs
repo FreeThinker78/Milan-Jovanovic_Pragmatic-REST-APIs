8f90895b8a591f93dd848549ecf559d56135317c
Joe Menendez <freethinker78@protonmail.com>
2025-07-18 00:37:56 -0400

    On main: 2.10 - Deleting Resources -  DELETE - Added DeleteHabit endpoint method

--- DevHabit/DevHabit.Api/Controllers/HabitsController.cs
+++ DevHabit/DevHabit.Api/Controllers/HabitsController.cs
@@ -104,4 +104,22 @@ public sealed class HabitsController(
 
         return NoContent();
     }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteHabit(string id)
+    {
+        Habit? habit = await dbContext.Habits.FirstOrDefaultAsync(h => h.Id == id);
+
+        if (habit is null)
+        {
+            //return StatusCode(StatusCodes.Status410Gone); To use this you must track deletes to find the id previously deleted.
+            return NotFound();
+        }
+
+        dbContext.Habits.Remove(habit);
+
+        await dbContext.SaveChangesAsync();
+
+        return NoContent();
+    }
 }
