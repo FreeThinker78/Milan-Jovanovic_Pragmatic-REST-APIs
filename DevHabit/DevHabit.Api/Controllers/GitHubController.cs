f416223694401dafe307935158ea27749fc005ca
Joe Menendez <freethinker78@protonmail.com>
2025-08-01 03:42:49 -0400

    4.11 - Integrating With External APIs - Created GitHubService and GitHubAccessTokenService - Registered the service with Dependency Injection - Created GitHubAccessToken entity and configuration - Added a database migration - Added StoreGitHubAccessTokenDto, GitHubUserProfileDto, GitHubEventDto - Added GitHubController with methods to store, and revoke the GitHub PAT token, and to Get your GitHub profile.

---
+++ DevHabit/DevHabit.Api/Controllers/GitHubController.cs
@@ -0,0 +1,91 @@
+﻿using System.Net.Mime;
+using DevHabit.Api.DTOs.Common;
+using DevHabit.Api.DTOs.GitHub;
+using DevHabit.Api.Entities;
+using DevHabit.Api.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevHabit.Api.Controllers;
+
+[Authorize(Roles = Roles.Member)]
+[ApiController]
+[Route("github")]
+public sealed class GitHubController(
+    GitHubAccessTokenService gitHubAccessTokenService,
+    GitHubService gitHubService,
+    UserContext userContext,
+    LinkService linksService)
+    : ControllerBase
+{
+    [HttpPut("personal-access-token")]
+    public async Task<IActionResult> StoreAccessToken(StoreGitHubAccessTokenDto storeGitHubAcccessTokenDto)
+    {
+        string? userId = await userContext.GetUserIdAsync();
+
+        if (string.IsNullOrWhiteSpace(userId))
+        {
+            return Unauthorized();
+        }
+
+        await gitHubAccessTokenService.StoreAsync(userId, storeGitHubAcccessTokenDto);
+
+        return NoContent();
+    }
+
+    [HttpDelete("personal-access-token")]
+    public async Task<IActionResult> RevokeAccessToken()
+    {
+        string? userId = await userContext.GetUserIdAsync();
+        if (string.IsNullOrWhiteSpace(userId))
+        {
+            return Unauthorized();
+        }
+
+        await gitHubAccessTokenService.RevokeAsync(userId);
+
+        return NoContent();
+    }
+
+    [HttpGet("profile")]
+    [Produces(
+        MediaTypeNames.Application.Json,
+        CustomMediaTypeNames.Application.JsonV1,
+        CustomMediaTypeNames.Application.JsonV2,
+        CustomMediaTypeNames.Application.HateoasJson,
+        CustomMediaTypeNames.Application.HateoasJsonV1,
+        CustomMediaTypeNames.Application.HateoasJsonV2)]
+
+    public async Task<ActionResult<GitHubUserProfileDto>> GetUserProfile([FromHeader] AcceptHeaderDto acceptHeader)
+    {
+        string? userId = await userContext.GetUserIdAsync();
+        if (string.IsNullOrWhiteSpace(userId))
+        {
+            return Unauthorized();
+        }
+
+        string? accessToken = await gitHubAccessTokenService.GetAsync(userId);
+        if (string.IsNullOrWhiteSpace(accessToken))
+        {
+            return NotFound();
+        }
+
+        GitHubUserProfileDto? userProfile = await gitHubService.GetUserProfileAsync(accessToken);
+        if (userProfile is null)
+        {
+            return NotFound();
+        }
+
+        if (acceptHeader.IncludeLinks)
+        {
+            userProfile.Links =
+            [
+                linksService.Create(nameof(GetUserProfile), "self", HttpMethods.Get),
+                linksService.Create(nameof(StoreAccessToken), "store-token", HttpMethods.Put),
+                linksService.Create(nameof(RevokeAccessToken), "revoke-token", HttpMethods.Delete)
+            ];
+        }
+
+        return Ok(userProfile);
+    }
+}
--- DevHabit/DevHabit.Api/DependencyInjection.cs
+++ DevHabit/DevHabit.Api/DependencyInjection.cs
@@ -21,6 +21,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using DevHabit.Api.Settings;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using System.Net.Http.Headers;
 
 namespace DevHabit.Api;
 
@@ -151,6 +152,21 @@ public static class DependencyInjection
         builder.Services.AddMemoryCache();
         builder.Services.AddScoped<UserContext>();
 
+        builder.Services.AddScoped<GitHubAccessTokenService>();
+        builder.Services.AddTransient<GitHubService>();
+        builder.Services
+            .AddHttpClient("github")
+            .ConfigureHttpClient(client =>
+            {
+                client.BaseAddress = new Uri("https://api.github.com");
+
+                client.DefaultRequestHeaders
+                    .UserAgent.Add(new ProductInfoHeaderValue("DevHabit", "1.0"));
+
+                client.DefaultRequestHeaders
+                    .Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github+json"));
+            });
+
         return builder;
     }
 
@@ -172,7 +188,7 @@ public static class DependencyInjection
             })
             .AddJwtBearer(options =>
             {
-                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                options.TokenValidationParameters = new TokenValidationParameters
                 {
                     ValidIssuer = jwtAuthOptions.Issuer,
                     ValidAudience = jwtAuthOptions.Audience,
